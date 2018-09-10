aa5b30a49e7980f724e1e1d3bafa7076cc7a4cbe
Per Engström <pengstrom@Pers-MacBook-Pro.local>
2018-09-10 14:40:35 +0200

    Roll with metadata

--- lib/dice.ex
+++ lib/dice.ex
@@ -1,9 +1,16 @@
 defmodule Dice do
+  @moduledoc """
+  Creating and rolling dice.
+  """
 
   @type const :: {:const, pos_integer}
   @type dice :: {:dice, pos_integer, pos_integer, pos_integer}
   @type roll :: const | dice
 
+  @doc """
+  Creates a s-sided die meant to be rolled n times and the result multiplied
+  by m.
+  """
   @spec dice(pos_integer, pos_integer, pos_integer) :: dice
   def dice(s \\ 6, n \\ 1, m \\ 1) do
     {:dice, s || 6, n || 1, m || 1}
@@ -28,4 +35,15 @@ defmodule Dice do
     |> Enum.map(&roll_one/1)
     |> Enum.concat
   end
+
+  def roll(parsed) do
+    parsed
+    |> Enum.map(&roll_parsed/1)
+  end
+
+  defp roll_parsed({:ok, res, spec}) do
+    [x] = roll_one(res)
+    {:ok, {x, res, spec}}
+  end
+  defp roll_parsed(parsed), do: parsed
 end
